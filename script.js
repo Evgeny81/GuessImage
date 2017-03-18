53b7d3f5c7563ca01fb792e3424ad1d8885ad337
kuznetsov_e <jackk@yandex.ru>
2017-03-18 13:08:26 +0300

    On mouse over and out small changes.

--- script.js
+++ script.js
@@ -8,17 +8,18 @@
     function init() {
         let images = document.getElementsByTagName('img');
         for (let i=0; i < images.length; i++) {
-            images[i].onclick = showAnswer;
+            images[i].onmouseover = showAnswer;
+            images[i].onmouseout = reblur;
         }
     }
     function showAnswer(eventObj) {
         let image = eventObj.target;
         let name = image.id;
         image.src = "./image/" + name + ".jpg";
-        setTimeout(reblur, 2000, image);
     }
 
-    function reblur(image) {
+    function reblur(eventObj) {
+        let image = eventObj.target;
         let name = image.id;
         image.src = "./image/" + name + "blur.jpg";
 
