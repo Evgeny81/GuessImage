dc8fdc0ce122e7788c476348b20f615c3f2db132
kuznetsov_e <jackk@yandex.ru>
2017-03-22 21:00:10 +0300

    Closures example.

--- script.js
+++ script.js
@@ -8,21 +8,18 @@
     function init() {
         let images = document.getElementsByTagName('img');
         for (let i=0; i < images.length; i++) {
-            images[i].onmouseover = showAnswer;
-            images[i].onmouseout = reblur;
+            images[i].onclick = showAnswer;
         }
     }
     function showAnswer(eventObj) {
         let image = eventObj.target;
         let name = image.id;
         image.src = "./image/" + name + ".jpg";
+        setTimeout(function () {
+            image.src = "./image/" + name + "blur.jpg";
+        }, 2000);
     }
 
-    function reblur(eventObj) {
-        let image = eventObj.target;
-        let name = image.id;
-        image.src = "./image/" + name + "blur.jpg";
 
-    }
 }());
 
