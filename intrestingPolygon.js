6e3fa390c6f0f7dd7af3e44f64a3bdf9f399e3fa
kuznetsov_e <jackk@yandex.ru>
2017-04-11 22:39:30 +0300

    TODOs were made.

--- intrestingPolygon.js
+++ intrestingPolygon.js
@@ -5,5 +5,8 @@
 
 function shapeArea(n) {
     "use strict";
-    
+    //TODO: check if n is greater than 0
+
+    //TODO: make function to produce line of a polygon
+    //TODO: push all lines in an array
 }
